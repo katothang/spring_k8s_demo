cf5d7cbb10a4d181c9b6ec3a06173693b94d71e8
Ku Thang <katothang@gmail.com>
2023-07-24 16:17:05 +0700

    update code

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("")
 public class DemoController {
     @GetMapping("")
-    public String getInfo() {
-        return "Chúng ta của quá khứ";
+    public String getInfo() throws UnknownHostException {
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        return "Demo k8s cluster with 3 nodes runing with ip = "+ip;
     }
 }
