c554d0eb31ca21bde85d9231936f1c7c7fbd5cec
Ku Thang <katothang@gmail.com>
2023-07-28 16:46:07 +0700

    update code

--- src/main/java/com/demo/job/LeaderElection.java
+++ src/main/java/com/demo/job/LeaderElection.java
@@ -17,6 +17,7 @@ public class LeaderElection {
 
     private final String LEADER_KEY = "leader";
     private final String LEADER_STATUS_KEY = "is_leader";
+    private final long LEASE_TIMEOUT = 10; // Lease timeout in seconds
 
     private final KV kv;
     private final Lease lease;
@@ -36,12 +37,18 @@ public class LeaderElection {
             long leaseId = grantLease();
 
             // Cố gắng giữ khóa lãnh đạo với leaseId được cấp phát trong khoảng thời gian lease
-            kv.put(ByteSequence.from(LEADER_KEY, StandardCharsets.UTF_8), ByteSequence.from("your-app", StandardCharsets.UTF_8), PutOption.newBuilder().withLeaseId(leaseId).build()).get();
-
-            // Lưu trạng thái lãnh đạo vào etcd (set giá trị "true" cho key "is_leader")
-            kv.put(ByteSequence.from(LEADER_STATUS_KEY, StandardCharsets.UTF_8), ByteSequence.from("true", StandardCharsets.UTF_8)).get();
-
-            return true;
+            PutResponse response = kv.put(ByteSequence.from(LEADER_KEY, StandardCharsets.UTF_8), ByteSequence.from("your-app", StandardCharsets.UTF_8), PutOption.newBuilder().withLeaseId(leaseId).build()).get();
+            if (response.hasPrevKv()) {
+                // Nếu key đã tồn tại, tức là không phải lãnh đạo
+                // Xóa trạng thái lãnh đạo trong etcd (set giá trị "false" cho key "is_leader")
+                kv.put(ByteSequence.from(LEADER_STATUS_KEY, StandardCharsets.UTF_8), ByteSequence.from("false", StandardCharsets.UTF_8)).get();
+                return false;
+            } else {
+                // Nếu key không tồn tại, chứng tỏ đã giành được lãnh đạo
+                // Lưu trạng thái lãnh đạo vào etcd (set giá trị "true" cho key "is_leader")
+                kv.put(ByteSequence.from(LEADER_STATUS_KEY, StandardCharsets.UTF_8), ByteSequence.from("true", StandardCharsets.UTF_8)).get();
+                return true;
+            }
         } catch (Exception e) {
             // Xử lý lỗi nếu có
             e.printStackTrace();
@@ -49,19 +56,8 @@ public class LeaderElection {
         }
     }
 
-    // Phương thức khi mất lãnh đạo
-    public void stepDown() {
-        try {
-            // Xóa trạng thái lãnh đạo trong etcd (set giá trị "false" cho key "is_leader")
-            kv.put(ByteSequence.from(LEADER_STATUS_KEY, StandardCharsets.UTF_8), ByteSequence.from("false", StandardCharsets.UTF_8)).get();
-        } catch (Exception e) {
-            // Xử lý lỗi nếu có
-            e.printStackTrace();
-        }
-    }
-
     private long grantLease() throws ExecutionException, InterruptedException {
-        LeaseGrantResponse leaseGrantResponse = lease.grant(10).get();
+        LeaseGrantResponse leaseGrantResponse = lease.grant(LEASE_TIMEOUT).get();
         return leaseGrantResponse.getID();
     }
 }
