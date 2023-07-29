da92401780f140358533cf1fbe0f819230916223
katothang <katothang@gmail.com>
2023-07-30 02:20:05 +0700

    update

--- src/main/java/com/demo/job/EtcdLeaderElection.java
+++ src/main/java/com/demo/job/EtcdLeaderElection.java
@@ -5,6 +5,7 @@ import io.etcd.jetcd.Client;
 import io.etcd.jetcd.KV;
 import io.etcd.jetcd.Lease;
 import io.etcd.jetcd.lease.LeaseKeepAliveResponse;
+import io.etcd.jetcd.options.PutOption;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -27,18 +28,20 @@ public class EtcdLeaderElection {
 
         // Tạo một UUID ngẫu nhiên cho mỗi instance và sử dụng nó làm khóa.
         String instanceId = UUID.randomUUID().toString();
-        ByteSequence key = ByteSequence.from(instanceId.getBytes(StandardCharsets.UTF_8));
-        ByteSequence value = ByteSequence.from("This is the leader".getBytes(StandardCharsets.UTF_8));
+        ByteSequence key = ByteSequence.from("leader-election".getBytes(StandardCharsets.UTF_8));
+        ByteSequence value = ByteSequence.from(instanceId.getBytes(StandardCharsets.UTF_8));
 
-        kvClient.put(key, value).get();
+        // Tạo một lease với thời gian tồn tại là 60 giây
+        long leaseId = leaseClient.grant(60).get().getID();
+
+        // Đăng ký khóa "leader-election" với UUID hiện tại và lease đã tạo
+        kvClient.put(key, value, PutOption.newBuilder().withLeaseId(leaseId).build()).get();
 
         // Gửi "KeepAlive" để giữ lease sống.
         new Thread(() -> {
             while (true) {
-                LeaseKeepAliveResponse response;
                 try {
-                    long leaseId = leaseClient.grant(60).get().getID();
-                    response = leaseClient.keepAliveOnce(leaseId).get();
+                    LeaseKeepAliveResponse response = leaseClient.keepAliveOnce(leaseId).get();
                     long ttl = response.getTTL();
                     if (ttl <= 0) {
                         // Lease hết hạn, tức là ứng dụng không còn là leader.
@@ -46,7 +49,7 @@ public class EtcdLeaderElection {
                     } else if (!isLeader) {
                         // Chưa phải leader và lease vẫn còn hiệu lực, tức là ứng dụng trở thành leader.
                         isLeader = true;
-                        System.out.println("I'm the leader! Instance ID: " + leaseId);
+                        System.out.println("I'm the leader! Instance ID: " + instanceId);
                     }
                     Thread.sleep(ttl * 1000 / 2);
                 } catch (Exception e) {
