650d2cfadb61839f171814d1d051099223453975
katothang <katothang@gmail.com>
2023-07-30 02:43:40 +0700

    update

--- src/main/java/com/demo/job/EtcdLeaderElection.java
+++ src/main/java/com/demo/job/EtcdLeaderElection.java
@@ -4,12 +4,14 @@ import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
 import io.etcd.jetcd.KV;
 import io.etcd.jetcd.Lease;
+import io.etcd.jetcd.kv.GetResponse;
 import io.etcd.jetcd.lease.LeaseKeepAliveResponse;
 import io.etcd.jetcd.options.PutOption;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Component
@@ -28,14 +30,14 @@ public class EtcdLeaderElection {
 
         // Tạo một UUID ngẫu nhiên cho mỗi instance và sử dụng nó làm khóa.
         String instanceId = UUID.randomUUID().toString();
-        ByteSequence key = ByteSequence.from("leader-election".getBytes(StandardCharsets.UTF_8));
-        ByteSequence value = ByteSequence.from(instanceId.getBytes(StandardCharsets.UTF_8));
+        ByteSequence key1 = ByteSequence.from("leader-election12".getBytes(StandardCharsets.UTF_8));
+        ByteSequence key = ByteSequence.from(instanceId.getBytes(StandardCharsets.UTF_8));
 
         // Tạo một lease với thời gian tồn tại là 60 giây
         long leaseId = leaseClient.grant(60).get().getID();
 
         // Đăng ký khóa "leader-election" với UUID hiện tại và lease đã tạo
-        kvClient.put(key, value, PutOption.newBuilder().withLeaseId(leaseId).build()).get();
+        kvClient.put(key, key, PutOption.newBuilder().withLeaseId(leaseId).build()).get();
 
         // Gửi "KeepAlive" để giữ lease sống.
         new Thread(() -> {
@@ -43,13 +45,12 @@ public class EtcdLeaderElection {
                 try {
                     LeaseKeepAliveResponse response = leaseClient.keepAliveOnce(leaseId).get();
                     long ttl = response.getTTL();
-                    if (ttl <= 0) {
+                    GetResponse getFuture = kvClient.get(key).get();
+                    if (ttl > 0 && getFuture.getCount() > 0) {
                         // Lease hết hạn, tức là ứng dụng không còn là leader.
-                        isLeader = false;
-                    } else if (!isLeader) {
-                        // Chưa phải leader và lease vẫn còn hiệu lực, tức là ứng dụng trở thành leader.
                         isLeader = true;
-                        System.out.println("I'm the leader! Instance ID: " + instanceId);
+                    } else {
+                        isLeader = false;
                     }
                     Thread.sleep(ttl * 1000 / 2);
                 } catch (Exception e) {
