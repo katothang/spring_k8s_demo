7f8f02e192cdb54a8b79f544eebdaf5a53e1cd0b
katothang <katothang@gmail.com>
2023-07-29 17:30:41 +0700

    update

--- src/main/java/com/demo/job/LeaderElectionJob.java
+++ src/main/java/com/demo/job/LeaderElectionJob.java
@@ -1,13 +1,20 @@
 package com.demo.job;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LeaderElectionJob {
-
+    @Autowired
+    private LeaderElectionService electionService;
     @Scheduled(fixedRate = 5000)
     public void printHello() {
-        System.out.println("hello");
+        if(electionService.isLeader()) {
+            System.out.println("hello leader");
+        } else {
+            System.out.println("hello worker");
+        }
+
     }
 }
