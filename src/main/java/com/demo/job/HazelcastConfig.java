55ca0e5be0c20ba19d7a1001e466f4f7da54e645
katothang <katothang@gmail.com>
2023-07-30 23:29:18 +0700

    update

--- src/main/java/com/demo/job/HazelcastConfig.java
+++ src/main/java/com/demo/job/HazelcastConfig.java
@@ -1,6 +1,8 @@
 package com.demo.job;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +13,15 @@ public class HazelcastConfig {
     @Bean
     public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
-        // Add any necessary Hazelcast configuration here
+        NetworkConfig networkConfig = config.getNetworkConfig();
+
+        // Set up the TCP-IP join configuration
+        JoinConfig joinConfig = networkConfig.getJoin();
+        joinConfig.getTcpIpConfig().setEnabled(true);
+        joinConfig.getTcpIpConfig().addMember("54.168.244.189:5701");
+        joinConfig.getTcpIpConfig().addMember("13.230.230.230:5701");
+        joinConfig.getTcpIpConfig().addMember("54.95.182.209:5701");
+
         return Hazelcast.newHazelcastInstance(config);
     }
 }
--- src/main/java/com/demo/job/JobHazelcast.java
+++ src/main/java/com/demo/job/JobHazelcast.java
@@ -2,70 +2,49 @@ package com.demo.job;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.HazelcastInstanceAware;
-import com.hazelcast.map.EntryProcessor;
 import com.hazelcast.map.IMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class JobHazelcast implements HazelcastInstanceAware {
     @Autowired
-    HazelcastInstance hazelcastInstance;
+    private HazelcastInstance hazelcastInstance;
 
     private static final String LEADER_MAP_KEY = "leader";
 
-    @Scheduled(fixedRate = 5000) // Chạy sau mỗi 5 giây
-    public void printLeaderStatus() {
+    @Scheduled(fixedRate = 5000) // In ra 5 giây một lần
+    public void printLeaderStatus() throws InterruptedException {
         IMap<String, String> leaderMap = hazelcastInstance.getMap(LEADER_MAP_KEY);
         String instanceId = hazelcastInstance.getLocalEndpoint().getUuid().toString();
+        boolean isLeader = false;
 
-        // Tạo một custom Entry Processor
-        SetLeaderEntryProcessor entryProcessor = new SetLeaderEntryProcessor(instanceId);
-
-        // Thực hiện đặt giá trị cho key "leader" và nhận kết quả trả về
-        boolean isLeader = leaderMap.executeOnKey(LEADER_MAP_KEY, entryProcessor);
+        // Thử đặt lock cho instance hiện tại
+        if (leaderMap.tryLock(instanceId, 0, TimeUnit.SECONDS)) {
+            try {
+                // Kiểm tra xem instance hiện tại có phải là leader không
+                if (leaderMap.get(LEADER_MAP_KEY) == null || leaderMap.get(LEADER_MAP_KEY).equals(instanceId)) {
+                    leaderMap.put(LEADER_MAP_KEY, instanceId);
+                    isLeader = true;
+                }
+            } finally {
+                // Giải phóng lock sau khi xử lý xong
+                leaderMap.unlock(instanceId);
+            }
+        }
 
         if (isLeader) {
-            System.out.println("Tôi là leader!");
+            System.out.println("I'm the leader!");
         } else {
-            System.out.println("Tôi là thành viên");
+            System.out.println("I'm the member");
         }
     }
 
     @Override
     public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
         this.hazelcastInstance = hazelcastInstance;
-    }
-
-    // Custom Entry Processor để đặt giá trị cho key "leader"
-    private static class SetLeaderEntryProcessor implements EntryProcessor<String, String, Boolean>, Serializable {
-        private final String instanceId;
-
-        public SetLeaderEntryProcessor(String instanceId) {
-            this.instanceId = instanceId;
-        }
-
-        @Override
-        public Boolean process(Map.Entry<String, String> entry) {
-            // Kiểm tra nếu key "leader" chưa có giá trị, hoặc giá trị là instanceId, thì đặt giá trị mới
-            if (entry.getValue() == null || entry.getValue().equals(instanceId)) {
-                entry.setValue(instanceId);
-                return true; // Trả về true nếu đặt giá trị thành công, chỉ có một instance làm leader
-            }
-            return false; // Trả về false nếu không đặt giá trị thành công
-        }
-
-        @Override
-        public EntryProcessor<String, String, Boolean> getBackupProcessor() {
-            return null;
-        }
-
-        public void processBackup(Map.Entry<String, String> entry) {
-            // Không cần xử lý sao lưu trong trường hợp này
-        }
     }
 }
